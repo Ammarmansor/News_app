cec07789e97907767d3179d0919d870d46adf573
PROJECT-TK17 <ammarthekiller17@gmail.com>
2024-11-02 21:27:56 +0300

    made news tile

--- lib/Screens/Home.dart
+++ lib/Screens/Home.dart
@@ -6,8 +6,7 @@ class HomeScreen extends StatelessWidget {
   HomeScreen({super.key});
   final List<CategoryModel> categoryModel = [
     CategoryModel(name: 'Business', imagetext: 'assets/business.avif'),
-    CategoryModel(
-        name: 'Entertainment', imagetext: 'assets/entertaiment.avif'),
+    CategoryModel(name: 'Entertainment', imagetext: 'assets/entertaiment.avif'),
     CategoryModel(name: 'General', imagetext: 'assets/general.avif'),
     CategoryModel(name: 'Health', imagetext: 'assets/health.avif'),
     CategoryModel(name: 'Science', imagetext: 'assets/science.avif'),
@@ -16,16 +15,43 @@ class HomeScreen extends StatelessWidget {
   ];
   @override
   Widget build(BuildContext context) {
-    
-    return SizedBox(
-      height: 100,
-      child: ListView.builder(
-        scrollDirection: Axis.horizontal,
-        itemCount: categoryModel.length,
-        itemBuilder: (context, index) {
-          return CategoryGenerator(categoryModel: categoryModel[index]);
-        },
-      ),
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: [
+        SizedBox(
+          height: 100,
+          child: ListView.builder(
+            scrollDirection: Axis.horizontal,
+            itemCount: categoryModel.length,
+            itemBuilder: (context, index) {
+              return CategoryGenerator(categoryModel: categoryModel[index]);
+            },
+          ),
+        ),
+        Column(
+          children: [
+            const SizedBox(
+              height: 10,
+            ),
+            ClipRRect(
+              borderRadius: const BorderRadius.all(Radius.circular(8)),
+              child: Image.asset('assets/business.avif'),
+            ),
+            const Text(
+              'dsfkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk',
+              maxLines: 2,
+              overflow: TextOverflow.ellipsis,
+              style: TextStyle(fontSize: 24),
+            ),
+            const Text(
+              'dsfkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk',
+              maxLines: 2,
+              overflow: TextOverflow.ellipsis,
+              style: TextStyle(fontSize: 12, color: Colors.grey),
+            )
+          ],
+        )
+      ],
     );
   }
 }
