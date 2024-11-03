4a1cdd0504742889afe7a83f405c9635993495a0
PROJECT-TK17 <ammarthekiller17@gmail.com>
2024-11-04 00:03:32 +0300

    finish

---
+++ lib/models/NewsTileModel.dart
@@ -0,0 +1,8 @@
+class NewsTileModel {
+
+final String title, description;
+final String? imageurl;
+
+  NewsTileModel({required this.title, required this.description, required this.imageurl});
+  
+}
